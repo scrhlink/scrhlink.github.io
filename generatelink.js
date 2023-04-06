7ecb515606d88968db52ab8df7897d8ca41854a5
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-06 10:02:57 +0100

    Update generatelink.js

--- generatelink.js
+++ generatelink.js
@@ -1,6 +1,8 @@
 while (true) {
   var inputusername = document.getElementById("uname");
-  var username = document.inputusername.value;
-  var url = `https://srchlink.github.io/u/${uname}`;
-  document.getElementById("scrh").textContent = url;
+  if (inputusername !== undefined) {
+    var username = document.inputusername.value;
+    var url = `https://srchlink.github.io/u/${uname}`;
+    document.getElementById("scrh").textContent = url;
+  }
 }
