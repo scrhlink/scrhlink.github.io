1fe5c61326e60acd3b966764e5c82c2b49cf3c2e
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-04-06 09:56:54 +0100

    Update generatelink.js

--- generatelink.js
+++ generatelink.js
@@ -1,5 +1,6 @@
-function makeUrl() {
-  var username = document.getElementById("uname").value;
+while (true) {
+  var inputusername = document.getElementById("uname");
+  var username = document.inputusername.value;
   var url = `https://srchlink.github.io/u/${uname}`;
   document.getElementById("scrh").textContent = url;
 }
